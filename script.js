16efb4b3eaa441456894ffa45d3aa6d5367412ff
karanacharya <karanacharya0987@gmail.com>
2024-06-24 18:28:47 +0530

    navbar-icon visibility problem solved
    And along with that some more images are added to make it look beautiful

--- script.js
+++ script.js
@@ -61,6 +61,23 @@ windchill_c
 windchill_f
 
 
+{/* <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav mr-auto">
+  <li class="nav-item active">
+    <a class="nav-link" href="About.html">About <span class="sr-only">(current)</span></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+ 
+</ul>
+<form class="form-inline my-2 my-lg-0">
+  <input class="form-control mr-sm-2" id="city" type="search" placeholder="Search" aria-label="Search">
+  <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="submit">Search</button>
+</form>
+</div> */}
+
+
 
 
 
